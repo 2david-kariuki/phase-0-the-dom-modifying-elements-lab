d616e303b592e1d5c6385229605705953d49a9b8
David <david48kariuki@gmail.com>
2025-03-20 23:00:14 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+// index.js
+
+
+const mainElement = document.querySelector("main#main");
+if (mainElement) {
+  mainElement.remove();
+}
+
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+newHeader.innerHTML = "YourName is the champion"; 
+document.body.append(newHeader);
+
+newHeader.style.fontSize = "24px";
+newHeader.style.margin = "20px";
